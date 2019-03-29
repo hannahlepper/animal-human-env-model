456ac98c3a38b62ec168aee5fc3699dd6dd8e246
hannahlepper <hannah.c.lepper@gmail.com>
2019-03-29 11:10:14 +0000

    init R FAST sensitivity analysis

---
+++ FASTsensitivity.R
@@ -0,0 +1,73 @@
+# FAST sensitivity analysis
+library(fast)
+library(sensitivity)
+library(readr)
+library(ggplot2)
+library(RColorBrewer)
+
+#Generate parameter values
+p_value_names <- c("LamH", "LamA", "gamH", "gamA",
+                   "betHH", "betAA",
+                   "betAH", "betEH",
+                   "betHA", "betEA",
+                   "betHE", "betAE",
+                   "muH", "muA", "muE")
+para_model = fast_parameters(minimum = c(rep(0, 14), 0.000001),
+                             maximum = rep(1, 15),
+                             factor = 9,
+                             names = p_value_names)
+write.csv(para_model, "M:/New projects/Model env compartment/paramodel.csv")
+
+#for wider environmental range
+para_model = fast_parameters(minimum = c(rep(0, 14), 0.000001),
+                             maximum = c(1, 1, 10, 10, 1, 1, 1, 10, 1, 10, 10, 10, 1, 1, 10),
+                             factor = 9,
+                             names = p_value_names)
+write.csv(para_model, "M:/New projects/Model env compartment/paramodel.csv")
+
+#Equilibrium for each parameter combination in Mathematica
+eq_analysis_bounded <- unlist(read_csv("M:/New projects/Model env compartment/outcomeequilibriumBounded.csv", 
+                               col_names = FALSE))
+eq_analysis_unbounded <- unlist(read_csv("M:/New projects/Model env compartment/outcomeequilibriumUnbounded.csv", 
+                               col_names = FALSE))
+
+#sensitivity analysis
+sens_bounded <- sensitivity(x = eq_analysis_bounded, 
+                    numberf = 15, 
+                    make.plot = F, 
+                    names = p_value_names)
+df.equilibrium_bounded <- data.frame(parameter = factor(p_value_names, ordered = TRUE), 
+                             value = sens_bounded)
+sens_unbounded <- sensitivity(x = eq_analysis_unbounded, 
+                            numberf = 15, 
+                            make.plot = F, 
+                            names = p_value_names)
+df.equilibrium_unbounded <- data.frame(parameter = factor(p_value_names), 
+                                     value = sens_unbounded)
+
+p <- ggplot(df.equilibrium_bounded, aes(parameter, value))
+p <- ggplot(df.equilibrium_unbounded, aes(parameter, value))
+
+
+p + geom_bar(stat="identity", fill=brewer.pal(3,"Set1")[2]) + 
+  scale_x_discrete("Parameter", waiver(),  
+                   c(expression(paste(beta[AA])),
+                     expression(paste(beta[AE])),
+                     expression(paste(beta[AH])),
+                     expression(paste(beta[EA])),
+                     expression(paste(beta[EH])),
+                     expression(paste(beta[HA])),
+                     expression(paste(beta[HE])),
+                     expression(paste(beta[HH])),
+                     expression(paste(gamma[A])),
+                     expression(paste(gamma[H])),
+                     expression(paste(Lambda[A])),
+                     expression(paste(Lambda[H])),
+                     expression(paste(mu[A])),
+                     expression(paste(mu[E])),
+                     expression(paste(mu[H])))) +
+  ylab("Partial Variance") + 
+    theme_bw() + 
+    theme(axis.text=element_text(size = 16, colour = "black"), axis.title=element_text(size=20)) +
+  labs(title = "Bounded model FAST")
+
