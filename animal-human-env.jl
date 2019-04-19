efa8f6c6af9b1154ffdfdab0d96fd47caa2f94c4
hannahlepper <hannah.c.lepper@gmail.com>
2019-04-19 14:42:54 +0100

    replace Lambda(E) with gammas

--- animal-human-env.jl
+++ animal-human-env.jl
@@ -3,56 +3,54 @@ using Plots
 
 #struct so parameters are easier to read/modify
 mutable struct ps_struct{T}
-  lH    ::T
-  lA    ::T
-  lE    ::T
-  muH   ::T
-  muA   ::T
-  muE   ::T
-  SH    ::T
-  SA    ::T
-  NH    ::T
-  N_A   ::T
+ lH    ::T
+ lA    ::T
 
-  bHH  ::T
-  bAA  ::T
+ gH    ::T
+ gA    ::T
 
-  bAH  ::T
-  bHA  ::T
+ bHH  ::T
+ bAA  ::T
 
-  bEH  ::T
-  bHE  ::T
+ bAH  ::T
+ bEH  ::T
 
-  bAE  ::T
-  bEA  ::T
+ bHA  ::T
+ bEA  ::T
+
+ bHE  ::T
+ bAE  ::T
+
+ muH   ::T
+ muA   ::T
+ muE   ::T
 end
 
-#fill with values matching B + W 2017 low impact scenario
-p = ps_struct{Float16}(
-  0.1, 0.1, 0.0,
-  0.1, 0.1, 0.0,
-  000.0, 000.0,
-  1., 1.,
-  0.1, 0.1,
-  0.1, 0.1,
-  0.0, 0.0,
-  0.0, 0.0)
+#fill with base values
+p = ps_strcut{Float16}(
+ 0.1, 0.1,
+ 0.1, 0.1,
+ 0.1, 0.1,
+ 0.1, 0.01,
+ 0.1, 0.01,
+ 0.1, 0.1,
+ 0.1, 0.1, 0.2)
 
 # differential equations
 function an_hum_env(du, u, p, t)
-  du[1] =  ((p.NH - u[1]) * (p.lH + (p.bHH * u[1]) / p.NH + (p.bAH * u[2]) / p.NH + p.bEH * u[3])) - p.muH * u[1]
-  du[2] =  ((p.N_A - u[2]) * (p.lA + (p.bAA * u[2]) / p.N_A + (p.bHA * u[1]) / p.N_A + p.bEA * u[3])) - p.muA * u[2]
-  du[3] =  p.lE + p.SH * p.bHE * u[1] + p.SA * p.bAE * u[2] - p.muE * u[3]
+ du[1] =  (1 - u[1]) * (p.lH + p.bHH * u[1] + p.bAH * u[2] + p.bEH * u[3]) - p.muH * u[1]
+ du[2] =  (1 - u[2]) * (p.lA + p.bAA * u[2] + p.bHA * u[1] + p.bEA * u[3]) - p.muA * u[2]
+ du[3] =  p.gA * p.lA + p.gH * p.lH + p.bHE * u[1] + p.bAE * u[2] - p.muE * u[3]
 end
 
-function an_hum_env(du, u, p, t)
-  du[1] =  (1 - u[1]) * ((p.lH + (p.bHH * u[1])  + (p.bAH * u[2])  + p.bEH * u[3])) - p.muH * u[1]
-  du[2] =  (1 - u[2]) * ((p.lA + (p.bAA * u[2])  + (p.bHA * u[1])  + p.bEA * u[3])) - p.muA * u[2]
-  du[3] =  p.lE + p.SH * p.bHE * u[1] + p.SA * p.bAE * u[2] - p.muE * u[3]
+function an_hum_env_b(du, u, p, t)
+  du[1] =  ((1 - u[1]) * (p.lH + (p.bHH * u[1]) + (p.bAH * u[2]) + p.bEH * u[3])) - p.muH * u[1]
+  du[2] =  ((1 - u[2]) * (p.lA + (p.bAA * u[2]) + (p.bHA * u[1]) + p.bEA * u[3])) - p.muA * u[2]
+  du[3] =  ((1 - u[3]) * (p.gA * p.lA + p.gH * p.lH + p.bHE * u[1] + p.bAE * u[2])) - p.muE * u[3]
 end
 
 # initial values
-u0 = [.7000; .7000; 00.70]
+u0 = [.0; .0; .0]
 
 # min and max time steps
 tspan = (0.0, 200.0)
@@ -60,9 +58,11 @@ tspan = (0.0, 200.0)
 #test
 an_hum_env_prob = ODEProblem(an_hum_env, u0, tspan, p)
 an_hum_env_sol = solve(an_hum_env_prob)
-
 plot(an_hum_env_sol)
-an_hum_env_sol(200)
+
+an_hum_env_b_prob = ODEProblem(an_hum_env_b, u0, tspan, p)
+an_hum_env_b_sol = solve(an_hum_env_b_prob)
+plot(an_hum_env_b_sol)
 
 # get Rh*
 function run_mod(p)
