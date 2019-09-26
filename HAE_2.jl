9b4ec13547fb034e0f394ad79a85a5f01cfbe6b2
hannahlepper <hannah.c.lepper@gmail.com>
2019-09-26 18:07:20 +0100

    Parameters for env. transmission scenario

--- HAE_2.jl
+++ HAE_2.jl
@@ -18,19 +18,39 @@ prob = ODEProblem(unboundeds,u0,tspan,p)
 @time sol = solve(prob)
 sol(500)
 
-#sample paramter space - dummy
+#sample paramter space
+N = 1000
+σ = 1
+#parameters of interest are bEH, LH, muE, muH.
 p_initial = zeros(1000, 15)
-for i in 1:15
-    p_initial[:,i] = rand(LogNormal(log(0.1), 0.05), 1000)
-end
+#Specify most probably value of LamH, then get lognormal distribution
+MPΛH = 0.1
+p_initial[:,1] .= rand(LogNormal(log(MPΛH)+σ, σ), N)
 
-#get rid of negative numbers
+MPβEH = 0.14
+p_initial[:,11].= rand(LogNormal(log(MPβEH)+σ,σ), N)
+
+MPμE = 0.2
+p_initial[:,15] .= rand(LogNormal(log(MPμE)+σ, σ), N)
+
+MPμH = 0.1
+p_initial[:,13] .= rand(LogNormal(log(MPμH)+σ, σ), N)
+
+#Rest of the parameters, on tha basis of the
+#ΛA and μA
+p_initial[:,[2,14]] .= 0.1
+#γH, γA, βHH, βAA, βHA, βAH
+p_initial[:,[3,4,5,6,7,8]] .= 0.001
+#βEA, βAE, βHE
+p_initial[:,[9,10,12]] .= 0.14
+
+#get rid of negative numbers, or values over 1? leave this out for now
 keep=[]
-for i in 1:1000
+@time for i in 1:1000
     if !(any(p_initial[i,:] .< 0.))
-        if !(any(p_initial[i,:] .> 1.))
+        #if !(any(p_initial[i,:] .> 1.))
             push!(keep, i)
-        end
+        #end
     end
 end
 
@@ -38,7 +58,7 @@ p = p_initial[keep, :]
 
 #now solve for each parameter set
 dat = zeros(size(p)[1], 16)
-for i in 1:size(p)[1]
+@time for i in 1:size(p)[1]
   dat[i, 1:15] .= p[i,:]
   prob = ODEProblem(unboundeds, u0, tspan, p[i,:])
   sol = solve(prob)
