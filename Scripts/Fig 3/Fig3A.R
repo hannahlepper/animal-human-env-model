c64476ffa07420d256d9eaed5171f18ded4bb4f6
hannahlepper <hannah.c.lepper@gmail.com>
2020-07-17 11:12:52 +0100

    get heatmaps working

--- Scripts/Fig 3/Fig3A.R
+++ Scripts/Fig 3/Fig3A.R
@@ -15,14 +15,46 @@ get_mean <- function(lower_bin, p, dat) {
 myP <- colorRampPalette(c("#070066","#1100FA","#428af5","#BAD3F7", "#F6FCBD", "#FFC803"),
                         bias = 1.)
 
-plot_heatmap <- function(df) {
+plot_heatmap <- function(df, title) {
   ggplot(df, aes(x_cat, y_cat, fill = mean)) +
     geom_tile() +
-    theme_bw(base_size = 5) +
+    theme_bw(base_size = 6) +
     theme(axis.text.x = element_text(angle = 90)) +
-    scale_fill_gradientn(expression(omega[A]), colors = myP(100), limits = c(0, 1)) +
-    ylab(expression(Lambda[A] ~ "(pre-intervention")) +
-    xlab(expression(beta[EH]))
+    scale_fill_gradientn(expression(omega[A]), colors = myP(100), limits = c(0, .15)) +
+    ylab(expression(Lambda[A] ~ "(pre-intervention)")) +
+    xlab(expression(beta[EH])) + 
+    labs(title = title)
 }
 
-p1 <- plot_heatmap(get_mean(lower_bin, P[[1]][[1]][,c(11,2)], d_impact[[1]]))
+p1 <- plot_heatmap(get_mean(lower_bin, P[[1]][[1]][,c(11,2)], d_impact[[1]]), "b")
+
+#Data for heatmaps needed
+impacts_highbHA <- d_impact
+impacts_lowbHA <- d_impact
+impacts_highbHA <- impact_2
+impacts_lowbHA <- impact_5
+
+TS <- c("Baseline", "Balanced", "Human\ndominated", "Environment\ndominated", "Animal\ndominated")
+plot_list_highbHA <- lapply(1:5, function(ts) {
+  mean_impact = get_mean(lower_bin, P[[3]][[ts]][,c(11,2)], impacts_highbHA[ts])
+  plot_heatmap(mean_impact, paste(c(TS[ts], " high bHA"), collapse = ""))
+})
+
+plot_list_lowbHA <- lapply(1:5, function(ts) {
+  mean_impact = get_mean(lower_bin, P[[3]][[ts]][,c(11,2)], impacts_lowbHA[ts])
+  plot_heatmap(mean_impact, paste(c(TS[ts], " low bHA")))
+})
+
+lapply(1:5, function(ts) {
+
+  fn <- paste(c("plots/heatmap_highbHA_", TS[ts]), collapse = "")
+  png(fn, width = 8, height = 7, units = "cm", res = 300)
+  print(plot_list_highbHA[[ts]])
+  dev.off()
+
+  fn <- paste(c("plots/heatmap_lowbHA_", TS[ts]), collapse = "")
+  png(fn, width = 8, height = 7, units = "cm", res = 300)
+  print(plot_list_lowbHA[[ts]])
+  dev.off()
+
+})
