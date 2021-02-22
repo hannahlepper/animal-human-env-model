d161c5db419b0c2fee2982e0f8ea534874211c8a
hannahlepper <hannah.c.lepper@gmail.com>
2021-02-22 18:14:30 +0000

    Update sensitivity plots

--- Scripts/FASTsensitivity.R
+++ Scripts/FASTsensitivity.R
@@ -1,9 +1,14 @@
 # FAST sensitivity analysis
-library(fast)
+library(fast) #Package gone
 library(sensitivity)
 library(readr)
 library(ggplot2)
 library(RColorBrewer)
+
+R_files <- list.files("/Users/hannah/Documents/fast/R")[-"sysdata.rda"]
+R_files <- sapply(R_files, function(f) paste(c("/Users/hannah/Documents/fast/R/", f), collapse = ""))
+for(f in R_files) source(f)
+load("/Users/hannah/Documents/fast/R/sysdata.rda")
 
 #Generate parameter values
 p_value_names <- c("LamH", "LamA", "gamH", "gamA",
@@ -26,9 +31,9 @@ para_model = fast_parameters(minimum = c(rep(0, 14), 0.000001),
 write.csv(para_model, "M:/New projects/Model env compartment/paramodel.csv")
 
 #Equilibrium for each parameter combination in Mathematica
-eq_analysis_bounded <- unlist(read_csv("M:/New projects/Model env compartment/outcomeequilibriumBounded.csv", 
+eq_analysis_bounded <- unlist(read_csv("data/outcomeequilibriumBounded.csv", 
                                col_names = FALSE))
-eq_analysis_unbounded <- unlist(read_csv("M:/New projects/Model env compartment/outcomeequilibriumUnbounded.csv", 
+eq_analysis_unbounded <- unlist(read_csv("data/outcomeequilibriumUnbounded.csv", 
                                col_names = FALSE))
 
 #sensitivity analysis
@@ -45,11 +50,11 @@ sens_unbounded <- sensitivity(x = eq_analysis_unbounded,
 df.equilibrium_unbounded <- data.frame(parameter = factor(p_value_names), 
                                      value = sens_unbounded)
 
-p <- ggplot(df.equilibrium_bounded, aes(parameter, value))
-p <- ggplot(df.equilibrium_unbounded, aes(parameter, value))
+p1 <- ggplot(df.equilibrium_bounded, aes(parameter, value))
+p2 <- ggplot(df.equilibrium_unbounded, aes(parameter, value))
 
 
-p + geom_bar(stat="identity", fill=brewer.pal(3,"Set1")[2]) + 
+p2 <- p2 + geom_bar(stat="identity", fill=brewer.pal(3,"Set1")[2]) + 
   scale_x_discrete("Parameter", waiver(),  
                    c(expression(paste(beta[AA])),
                      expression(paste(beta[AE])),
@@ -71,3 +76,10 @@ p + geom_bar(stat="identity", fill=brewer.pal(3,"Set1")[2]) +
     theme(axis.text=element_text(size = 16, colour = "black"), axis.title=element_text(size=20)) +
   labs(title = "Bounded model FAST")
 
+png("plots/Fig1C.A.png", width = 16, height = 10, units = "cm", res = 300)
+p1
+dev.off()
+
+png("plots/Fig1C.B.png", width = 16, height = 10, units = "cm", res = 300)
+p2
+dev.off()
