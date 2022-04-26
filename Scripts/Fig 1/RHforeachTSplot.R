74ec219f01f9167826f5a8a7598ff66e100f7e0e
hannahlepper <hannah.c.lepper@gmail.com>
2022-04-26 14:02:45 +0100

    change fig1B from all R to RE only

--- Scripts/Fig 1/RHforeachTSplot.R
+++ Scripts/Fig 1/RHforeachTSplot.R
@@ -1,18 +1,35 @@
 #sort out julia's list
 R <- unlist(R)
+R2 <- unlist(R2)
 #For testing - dummy data
 #R = rbeta(3 * 5, 1, 1)
 Rdf <- data.frame(
-        R = R,
-        R_type = factor(rep(c("R[H]", "R[A]", "R[E]"), times = 5)),
+        R = R, model = "Unbounded\nenvironment",
+        R_type = factor(rep(c("RH", "RA", "RE"), times = 5)),
         TS = rep(c("Baseline", "Balanced", "Environment\ndominated", "Animal\ndominated", "Human\ndominated"), each = 3))
 
+Rdf2 <- data.frame(
+        R = R2, model = "Bounded\nenvironment",
+        R_type = factor(rep(c("RH", "RA", "RE"), times = 5)),
+        TS = rep(c("Baseline", "Balanced", "Environment\ndominated", "Animal\ndominated", "Human\ndominated"), each = 3))
+
+Rdf <- bind_rows(Rdf, Rdf2) %>%
+    subset(., (R_type == "RE")) %>%
+    mutate(., TS = factor(TS, 
+                        levels = c("Baseline", "Balanced", "Human\ndominated", 
+                                    "Animal\ndominated", "Environment\ndominated"),
+                        ordered = TRUE),
+             R_type = factor(R_type, levels = c("RH", "RA", "RE"),
+                        labels = c("RH", "Proportion of animals carrying\n resistant bacteria",
+                                    "Proportion of environmental units\n carrying resistant bacteria")))
+
 png("plots/barplotFig1B.png", width = 8, height = 7, units = "cm", res = 300)
-p <- ggplot(Rdf, aes(TS, R, fill = R_type)) +
+p <- ggplot(Rdf, aes(TS, R, fill = model)) +
     geom_bar(stat = "identity", position = "dodge") +
-    scale_fill_discrete("", labels = parse(text = levels(Rdf$R_type))) +
-    labs(x = "", y = "Fraction of population carrying\n resistant bacteria") +
+    #scale_fill_discrete("", labels = parse(text = levels(Rdf$R_type))) +
+    labs(x = "", y = "", fill = "Model") +
     theme_bw(base_size = 8) +
-    theme(axis.text.x = element_text(angle = 45, hjust = 0.95))
+    theme(axis.text.x = element_text(angle = 45, hjust = 0.95)) +
+    facet_wrap(~R_type, ncol = 1)
 print(p)
 dev.off()
